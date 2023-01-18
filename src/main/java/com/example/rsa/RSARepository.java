030f236c737ac0a47da9c544452edea9a32de080
Lytov <misha.lytov@yande.xru>
2023-01-18 10:10:06 +0300

    SQL

--- src/main/java/com/example/rsa/RSARepository.java
+++ src/main/java/com/example/rsa/RSARepository.java
@@ -3,9 +3,12 @@ package com.example.rsa;
 import com.example.rsa.models.Post;
 import com.example.rsa.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Optional;
 
 @Repository
 public class RSARepository {
@@ -13,8 +16,14 @@ public class RSARepository {
     private PostRepository postRepository;
 
     public void save(String path, String text_in_file, BigInteger open_exhibitor,BigInteger close_exhibitor,BigInteger derivative){
-        Post post = new Post(path,text_in_file,open_exhibitor,close_exhibitor,derivative);
+        /*Post post = new Post(path,text_in_file,open_exhibitor,close_exhibitor,derivative);
+        post.setId(2l);*/
+        System.out.println(close_exhibitor);
+        //Post post = new Post(path, text_in_file, open_exhibitor.toString(),close_exhibitor.toString(), derivative.toString());
+        Post post = new Post(path, text_in_file, open_exhibitor.toString(),close_exhibitor.toString(), derivative.toString());
         postRepository.save(post);
+        System.out.println(post.getId());
+
     }
 
 
--- src/main/java/com/example/rsa/models/Post.java
+++ src/main/java/com/example/rsa/models/Post.java
@@ -7,7 +7,8 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigInteger;
+import java.io.File;
+
 @Getter
 @Setter
 @Entity
@@ -17,10 +18,9 @@ public class Post { //модель, создаёт табличку данных
     private Long id;
     private String path;
     private String text_in_file;
-
-    private BigInteger open_exhibitor, close_exhibitor, derivative;
-
-    public Post(String path, String text_in_file, BigInteger open_exhibitor, BigInteger close_exhibitor, BigInteger derivative) {
+    private String open_exhibitor,  derivative,  close_exhibitor;
+    //private String close_exhibitor = "10515633502775227735107121108518308219115772037878032934818544518386351171346578592019726881470777485027869017842062197800631638262874513401963986033240061762479895854447719811408329384317760680494647873389392582167924875619392468732758787541519622259264108751836280243846414294784847037467683458798552265293841979390148971920480192710826566412887525851511850067991276457023477600784841805770719509199805083466828056418929364319847829204954065502453246635524000173366674616460463030487585041758567913595675458292640520781807942768084453498841202120783020793257803418631400007008145596392413764270146760836082693191571";
+    public Post(String path, String text_in_file, String open_exhibitor, String close_exhibitor, String derivative) {
         this.path = path;
         this.text_in_file = text_in_file;
         this.open_exhibitor = open_exhibitor;
